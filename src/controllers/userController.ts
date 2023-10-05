16bc1c65ff47786af963668af9f6d8d3b16ee742
Mohit Saud <mohit@prabidhilabs.com>
2023-10-05 11:23:35 +0545

    Added :: register user controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,7 +1,19 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import User from '../models/User';
+
+//
+export const registerUser = async (req: Request, res: Response) => {
+  const user = await User.create({
+    email: 'themohitsaud@gmail.com',
+    role: 'super-admin',
+  });
+
+  res.status(StatusCodes.CREATED).json({ user });
+};
 
 export const getUsers = async (req: Request, res: Response) => {
   const users = ['Ram', 'Shyam', 'Sita', 'Hari'];
 
-  res.status(200).json({ users });
+  res.status(StatusCodes.OK).json({ users });
 };
