a47f7d61fcaa84988871d6137d593b709d1b4813
Mohit Saud <mohit@prabidhilabs.com>
2023-10-06 21:41:30 +0545

    Mount :: error handler and no found middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import dotenv from 'dotenv';
+import 'express-async-errors';
 import express, { Request, Response } from 'express';
+import compression from 'compression';
 
 import userRouter from './routes/userRoutes';
 import connectDB from './db/connect';
+import notFoundMiddleware from './middleware/not-found';
+import errorHandlerMiddleware from './middleware/error-handler';
 
 const app = express();
 
@@ -10,11 +14,18 @@ dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
+app.use(compression());
+
 app.get('/', (req: Request, res: Response) =>
   res.send('Welcome to Node Typescript tutorial')
 );
 
+// Mount the routers
 app.use('/api/v1/users', userRouter);
+
+// Middlewares
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const MONGO_URI: string | undefined = process.env.MONGO_URI;
 
